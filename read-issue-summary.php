0fd4c8dcea5f9492e17fc60e328039c360729d0a
Peter Kiraly <peter.kiraly@gwdg.de>
2019-03-24 15:15:22 +0100

    Order issues.

--- read-issue-summary.php
+++ read-issue-summary.php
@@ -7,6 +7,7 @@ $configuration = parse_ini_file("configuration.cnf");
 
 $elementsFile = sprintf('%s/%s/issue-summary.csv', $configuration['dir'], $db);
 $records = [];
+$types = [];
 $max = 0;
 if (file_exists($elementsFile)) {
   // $keys = ['path', 'type', 'message', 'url', 'count']; // "sum",
@@ -28,12 +29,31 @@ if (file_exists($elementsFile)) {
       $record = (object)array_combine($header, $values);
       $type = $record->type;
       unset($record->type);
+      $record->url = str_replace('https://www.loc.gov/marc/bibliographic/', '', $record->url);
       if (!isset($records[$type])) {
         $records[$type] = [];
       }
       $records[$type][] = $record;
     }
   }
+  $types = array_keys($records);
+  $locations = [];
+  foreach ($types as $type) {
+    list($location, $subtype) = explode(': ', $type);
+    if (!isset($locations[$location])) {
+      $locations[$location] = [];
+    }
+    $locations[$location][] = $type;
+  }
+  $orderedLocations = ['record', 'control subfield', 'field', 'indicator', 'subfield'];
+  $typesOrdered = [];
+  foreach ($orderedLocations as $location) {
+    if (isset($locations[$location])) {
+      asort($locations[$location]);
+      $typesOrdered = array_merge($typesOrdered, $locations[$location]);
+    }
+  }
+
 } else {
   $msg = sprintf("file %s is not existing", $elementsFile);
   error_log($msg);
@@ -42,5 +62,6 @@ if (file_exists($elementsFile)) {
 header("Content-type: application/json");
 echo json_encode([
   'records' => $records,
+  'types' => $typesOrdered
 ]);
 
