0ef53646165c6eb0a04690233143154b1828c4ba
Peter Kiraly <peter.kiraly@gwdg.de>
2019-03-26 13:18:33 +0100

    Add issue details reader.

---
+++ read-issue-details.php
@@ -0,0 +1,57 @@
+<?php
+require_once 'common-functions.php';
+
+set_time_limit(300);
+
+$db = getOrDefault('db', 'cerl');
+$type = getOrDefault('type', '');
+$path = getOrDefault('path', '');
+$message = getOrDefault('message', '');
+
+// error_log()
+
+$configuration = parse_ini_file("configuration.cnf");
+
+$elementsFile = sprintf('%s/%s/issue-details.csv', $configuration['dir'], $db);
+$recordIds = [];
+$types = [];
+$max = 0;
+if (file_exists($elementsFile)) {
+  // $keys = ['recordId', 'MarcPath', 'type', 'message', 'url']
+  $lineNumber = 0;
+  $header = [];
+  $in = fopen($elementsFile, "r");
+  while (($line = fgets($in)) != false) {
+    if (count($recordIds) < 10) {
+      $lineNumber++;
+      $values = str_getcsv($line);
+      if ($lineNumber == 1) {
+        $header = $values;
+        $header[1] = 'path';
+        error_log('header: ' . json_encode($header));
+      } else {
+        if ($lineNumber == 2)
+          error_log(count($header) . ' vs ' . count($values));
+        if (count($header) != count($values)) {
+          error_log('line #' . $lineNumber . ': ' . count($header) . ' vs ' . count($values));
+        }
+        $record = (object)array_combine($header, $values);
+        if ($record->path == $path
+          && $record->type == $type
+          && $record->message == $message) {
+          $recordIds[] = $record->recordId;
+        }
+      }
+    }
+  }
+  fclose($in);
+} else {
+  $msg = sprintf("file %s is not existing", $elementsFile);
+  error_log($msg);
+}
+
+header("Content-type: application/json");
+echo json_encode([
+  'recordIds' => $recordIds
+]);
+
