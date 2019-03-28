cb713896fec29da641509de17e6e7dec654eaf37
Peter Kiraly <peter.kiraly@gwdg.de>
2019-03-28 15:27:36 +0100

    Fixing facet settings.

--- listFields.php
+++ listFields.php
@@ -1,5 +1,8 @@
 <?php
-$url = 'http://localhost:8983/solr/gent';
+require_once 'common-functions.php';
+
+$db = getOrDefault('db', 'cerl');
+$url = 'http://localhost:8983/solr/' . $db;
 
 $all_fields = file_get_contents($url . '/select/?select?q=*:*&wt=csv&rows=0');
 $fields = explode(',', $all_fields);
