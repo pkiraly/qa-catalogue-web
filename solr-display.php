825fbe968f5a93403cb05ca9403ee2f80132af52
Peter Kiraly <peter.kiraly@gwdg.de>
2019-06-03 15:10:31 +0200

    Custom links for DNB.

--- solr-display.php
+++ solr-display.php
@@ -144,9 +144,8 @@ function opacLink($doc, $id) {
       }
     }
     return 'http://hpb.cerl.org/record/' . $identifier;
-  else if ($core == 'dnb')
-      return 'http://d-nb.info/' . trim($id);
-  }
+  } else if ($core == 'dnb')
+    return 'http://d-nb.info/' . trim($id);
 }
 
 /**
