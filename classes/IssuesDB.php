1f459731e66b178dc167476e111f8357019dd8d0
pkiraly <pkiraly@gwdg.de>
2021-10-21 21:49:52 +0200

    Adding groups and orders of the issues.

--- classes/IssuesDB.php
+++ classes/IssuesDB.php
@@ -52,15 +52,22 @@ class IssuesDB extends SQLite3 {
     if (!preg_match('/^(path|variants|instances|records) (ASC|DESC)$/', $order))
       $order = $default_order;
     $stmt = $this->prepare('
-SELECT s.MarcPath AS path, COUNT(DISTINCT(s.id)) AS variants, SUM(d.instances) AS instances, COUNT(DISTINCT(d.id)) AS records
-FROM issue_summary AS s
-LEFT JOIN issue_details AS d ON (s.id = d.errorId)
+SELECT path, variants, instances, records
+FROM issue_groups AS s
 WHERE categoryId = :categoryId AND typeId = :typeId
-GROUP BY s.MarcPath
 ORDER BY ' . $order . ';');
     $stmt->bindValue(':categoryId', $categoryId, SQLITE3_INTEGER);
     $stmt->bindValue(':typeId', $typeId, SQLITE3_INTEGER);
 
     return $stmt->execute();
   }
+
+/*
+SELECT s.MarcPath AS path, COUNT(DISTINCT(s.id)) AS variants, SUM(d.instances) AS instances, COUNT(DISTINCT(d.id)) AS records
+FROM issue_summary AS s
+LEFT JOIN issue_details AS d ON (s.id = d.errorId)
+WHERE categoryId = :categoryId AND typeId = :typeId
+GROUP BY s.MarcPath
+ORDER BY ' . $order . ';');
+*/
 }
