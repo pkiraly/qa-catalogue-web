95e1e3bab61ea563273269a786811c7be8d5768e
Péter Király <pkiraly@gwdg.de>
2022-10-16 22:41:41 +0200

    Adding Czech catalogues.

--- classes/catalogue/Nkp.php
+++ classes/catalogue/Nkp.php
@@ -9,8 +9,17 @@ class Nkp extends Catalogue {
   protected $marcVersion = 'NKCR';
 
   function getOpacLink($id, $record) {
+    $rid = FALSE;
+    foreach ($record->getFields('015') as $tag) {
+      if (isset($tag->subfields->a)) {
+        $rid = str_replace('cnb', '', $tag->subfields->a);
+        break;
+      }
+    }
+    if ($rid === FALSE)
+      $rid = preg_replace('/^[a-z]{1,3}\d{4}/', '00', trim($id))
     return 'https://aleph.nkp.cz/F/?func=direct'
-         . '&doc_number=' . preg_replace('/^[a-z]{1,3}\d{4}/', '00', trim($id))
+         . '&doc_number=' . $rid
          . '&local_base=CNB';
   }
 }
