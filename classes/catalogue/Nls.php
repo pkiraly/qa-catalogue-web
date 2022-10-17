0eda07911262de0bd1b26b8c2cb49231ab064392
Péter Király <pkiraly@gwdg.de>
2022-10-17 16:38:55 +0200

    Add configuration for National Bibliography of Scotland

--- classes/catalogue/Nls.php
+++ classes/catalogue/Nls.php
@@ -8,18 +8,7 @@ class Nls extends Catalogue {
   protected $url = 'https://www.nls.uk/';
 
   function getOpacLink($id, $record) {
-    $rid = FALSE;
-    foreach ($record->getFields('015') as $tag) {
-      if (isset($tag->subfields->a)) {
-        $rid = str_replace('cnb', '', $tag->subfields->a);
-        break;
-      }
-    }
-    if ($rid === FALSE)
-      $rid = preg_replace('/^[a-z]{1,3}\d{4}/', '00', trim($id));
-    return 'https://aleph.nkp.cz/F/?func=direct'
-         . '&doc_number=' . $rid
-         . '&local_base=CNB';
-    // https://search.nls.uk/permalink/f/19q5vbt/44NLS_ALMA21562138960004341
+    return 'https://search.nls.uk/primo-explore/search?query=any,contains,%22' 
+           . $id . '%22&tab=tab1_local&search_scope=SCOPE1&vid=44NLS_VU1&lang=en_US&offset=0'
   }
 }
