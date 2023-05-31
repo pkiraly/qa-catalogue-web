65939ef3a205bf1e8bb2d3cd1a4514038c9fcdeb
Nelles, David <david.nelles@gmx.de>
2023-05-31 16:57:06 +0200

    feat: Added a pie chart for issue summary

--- classes/Start.php
+++ classes/Start.php
@@ -1,9 +1,58 @@
 <?php
 
+include 'Issues.php';
+
 class Start extends BaseTab {
 
   public function getTemplate() {
     return 'start/start.tpl';
   }
 
+  public function prepareData(Smarty &$smarty) {
+    parent::prepareData($smarty);
+
+    $smarty->assign('issueStats', $this->readTotal());
+  }
+
+  public function readIssueCsv($filename, $keyField) {
+    if ($this->versioning && $this->version != '') {
+      $elementsFile = $this->getVersionedFilePath($this->version, $filename);
+    } else {
+      $elementsFile = $this->getFilePath($filename);
+    }
+    return readCsv($elementsFile, $keyField);
+  }
+
+  private function readTotal() {
+    if ($this->grouped) {
+      $statistics = $this->filterByGroup($this->readIssueCsv('issue-total.csv', ''), 'type');
+      $this->total = $this->currentGroup->count;
+    } else {
+      $statistics = $this->readIssueCsv('issue-total.csv', 'type');
+      $this->total = $this->count;
+    }
+
+    foreach ($statistics as &$item) {
+      $item->good = $this->total - $item->records;
+      $item->goodPercent = ($item->good / $this->total) * 100;
+      $item->bad = $item->records;
+      $item->badPercent = ($item->bad / $this->total) * 100;
+    }
+
+    if (!isset($statistics["0"]))
+      $statistics["0"] = (object)[
+        "type" => "0",
+        "instances" => "0",
+        "records" => "0",
+        "percent" => 0
+      ];
+
+    $complete = (object)[
+      "good" => $statistics[1]->good,
+      "unclear" => $statistics[2]->good - $statistics[1]->good,
+      "bad" => $statistics[2]->bad
+    ];
+
+    return $complete;
+  }
 }
---
+++ js/start.js.tpl
@@ -0,0 +1,21 @@
+const ctx = document.getElementById('issuesGraph');
+
+new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ['Without issues', 'With undefined fields', 'With issues'],
+        datasets: [{
+            label: '# of Records',
+            data: ['{$issueStats->bad|escape:javascript}', '{$issueStats->unclear|escape:javascript}', '{$issueStats->good|escape:javascript}'],
+            backgroundColor: ["#2ECC40", "#FFFF00", "#FF4136"],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
