75f69b4152501d80d6ab5e7afb026bd15ded62e6
Péter Király <pkiraly@gwdg.de>
2023-10-16 13:51:21 +0200

    display the number of data elements at the completeness tab #126: debug

--- classes/Completeness.php
+++ classes/Completeness.php
@@ -165,6 +165,7 @@ class Completeness extends BaseTab {
               $record->iscoretag = false;
             }
             if (isset($dataElementCounts[$record->packageid])) {
+              error_log('add data element counts to package #' . $record->packageid);
               $record->fieldCount = $dataElementCounts[$record->packageid]['fieldCount'];
               $record->subfieldCount = $dataElementCounts[$record->packageid]['subfieldCount'];
             }
@@ -186,6 +187,7 @@ class Completeness extends BaseTab {
             : 1);
       });
       if (isset($dataElementCounts['total'])) {
+        error_log('add data element counts to package total' . $record->packageid);
         $this->packages[] = (object)[
           'packageid' => 'total',
           'iscoretag' => false,
