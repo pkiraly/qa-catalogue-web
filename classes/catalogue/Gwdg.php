0b03a8aa8c68d11e40d5c584cfcb3862643f3571
Péter Király <pkiraly@gwdg.de>
2025-08-13 18:24:10 +0200

    Add GWDG opac link

--- classes/catalogue/Gwdg.php
+++ classes/catalogue/Gwdg.php
@@ -4,5 +4,20 @@ class Gwdg extends Catalogue {
   protected $name = 'gwdg';
   protected $label = 'GWDG';
   protected $url = 'https://www.gwdg.de/';
-  // protected $linkTemplate = 'https://test-mpis.koha.gwdg.de/cgi-bin/koha/opac-detail.pl?biblionumber={id}';
+
+  function getOpacLink($id, $record) {
+    $rid = FALSE;
+    foreach ($record->getFields('999') as $tag) {
+      if (isset($tag->subfields->c)) {
+        $rid = $tag->subfields->c;
+        break;
+      }
+    }
+
+    if ($rid !== FALSE)
+      return sprintf('https://test-mpis.koha.gwdg.de/cgi-bin/koha/opac-detail.pl?biblionumber=%s', $rid);
+    else
+      return null;
+  }
+
 }
