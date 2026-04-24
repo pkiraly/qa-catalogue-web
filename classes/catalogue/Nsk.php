4fcaae0629e85394262c3d63f20bad6482c89740
Péter Király <pkiraly@gwdg.de>
2026-04-24 23:01:06 +0200

    Adding Croatian Nat. Lib.

---
+++ classes/catalogue/Nsk.php
@@ -0,0 +1,9 @@
+<?php
+
+class Nsk extends Catalogue {
+  protected $name = 'nsk';
+  protected $label = 'National and University Library in Zagreb / Nacionalna i sveučilišna knjižnica u Zagrebu';
+  protected $url = 'https://www.nsk.hr/';
+  # protected $marcVersion = 'KBR';
+  protected $linkTemplate = 'https://katalog.nsk.hr/permalink/385BUKINET_ZANSK/ksmp77/alma{id}';
+}
