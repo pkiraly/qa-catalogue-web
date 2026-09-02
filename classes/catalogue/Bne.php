80f9deef5d035017f4406c2c51cf8ba76d843e62
Péter Király <pkiraly@gwdg.de>
2026-09-02 09:54:20 +0200

    Add bne.es related class

--- classes/catalogue/Bne.php
+++ classes/catalogue/Bne.php
@@ -2,7 +2,7 @@
 
 class Bne extends Catalogue {
   protected $name = 'bne';
-  protected $label = 'Verbundkatalog B3Kat des Bibliotheksverbundes Bayern (BVB) und des Kooperativen Bibliotheksverbundes Berlin-Brandenburg (KOBV)';
+  protected $label = 'Biblioteca Nacional de España';
   protected $url = 'https://www.bne.es/';
   protected $linkTemplate = 'https://catalogo.bne.es/permalink/34BNE_INST/12h209n/alma{id}';
 }
